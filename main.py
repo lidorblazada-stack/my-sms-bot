6fb2bbd92c6ecf93e66cc89394dfab3bf74b5c53
lidorblazada-stack <lidorblazada@gmail.com>
2026-08-19 06:22:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ class VerifyView(ui.View):
             )
             
             link_view = ui.View()
-            link_view.add_item(ui.Button(label="🔗 כניסה לשרת הגיבוי", url=BACKUP_INVITE_URL))
+            link_view.add_item(ui.Button(label="🔗 כניסה לשרת הגיבוי", url=https://discord.gg/MZedFgBB9y))
             return await i.response.send_message(embed=emb, view=link_view, ephemeral=True)
             
         await i.user.add_roles(role)
